188d2579b3450824954bda2008b50cfbbf094f0a
Gui-Rigaud <guirigaud@hotmail.com>
2025-11-30 00:28:58 -0300

    feat: add cutscene functionality and dialogue box

--- tiny swords/scenes/Areas/Hub.gd
+++ tiny swords/scenes/Areas/Hub.gd
@@ -1,9 +1,17 @@
 extends Node2D
 @export var gameui: CanvasLayer
 @export var game_over_ui: PackedScene
+const CutsceneIntroScene := preload("res://scenes/cutscene/Cutscene.tscn")
+
+@onready var cutscene_layer: CanvasLayer = $CutsceneLayer
 
 func _ready():
+	_show_cutscene_intro()
 	GameManager.game_over.connect(trigger_game_over)
+
+func _show_cutscene_intro() -> void:
+	var cutscene := CutsceneIntroScene.instantiate()
+	cutscene_layer.add_child(cutscene)
 
 func trigger_game_over():
 	if gameui:

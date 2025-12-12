00adb83d0bbf89ee3c86e502cc3aaac6de03e531
jpam <160938086+SunIord@users.noreply.github.com>
2025-12-12 09:54:56 -0300

    feat: attack animation for enemies

--- tiny swords/scenes/cutscene/Cutscene.gd
+++ tiny swords/scenes/cutscene/Cutscene.gd
@@ -5,7 +5,7 @@ extends Control
 
 var lines := [
 	"Olá, viajante...",
-	"Bem-vindo ao reino de A Goblin's Tale!",
+	"Bem-vindo ao reino de 'A Goblin's Tale'!",
 	"Há muitas gerações, o reino dos cavaleiros e o dos goblins viviam em paz graças a um antigo tratado que garantia terras e segurança para ambos. Com o tempo, porém, a ambição e o preconceito dos cavaleiros cresceram, e eles romperam o acordo, invadindo e tomando as terras dos goblins à força.", 
 	"Anos depois desse rompimento, um goblin simples viu sua vida desmoronar quando sua esposa e seu melhor amigo desapareceram sem deixar rastros. Desesperado e sem pistas, ele parte em uma jornada solitária em busca dos dois, cruzando florestas perigosas, ruínas de antigas vilas goblins e fronteiras proibidas entre os reinos.",
 	"Durante a viagem, o goblin descobre que sua vila foi atacada por uma horda de cavaleiros e que sua esposa e seu amigo foram sequestrados. Agora, ele não está apenas em busca dos entes queridos: precisa desvendar a verdade por trás do ataque, enfrentar o reino que traiu o tratado e se vingar dos cavaleiros que destruíram sua vida."
